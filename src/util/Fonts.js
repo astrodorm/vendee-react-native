87f9fbd7fbb8e3e74b81f26479bf61c69d52396d
michael_vons <mike2serg@gmail.com>
2019-01-27 02:16:55 +0100

    added  icons for ios, compiled for ios

--- src/util/Fonts.js
+++ src/util/Fonts.js
@@ -1,13 +1,22 @@
 const Fonts = {
-  JosefinThin: "JosefinSans-Thin",
-  JosefinRegular: "JosefinSans-Regular",
-  JosefinBold: "JosefinSans-Bold",
-  JosefinSemiBold: "JosefinSans-SemiBold",
-  MPLUSThin: "MPLUSRounded1c-Thin",
-  MPLUSRegular: "MPLUSRounded1c-Regular",
-  MPLUSBold: "MPLUSRounded1c-Bold",
-  MPLUSMedium: "MPLUSRounded1c-Medium",
-  MPLUSBlack: "MPLUSRounded1c-Black"
+  /******* */
+  //UNCOMMENT FOR IOS
+  /**** */
+  MPLUSThin: "Rounded Mplus 1c Thin",
+  MPLUSRegular: "Rounded Mplus 1c",
+  MPLUSBold: "Rounded Mplus 1c Bold",
+  MPLUSMedium: "Rounded Mplus 1c Medium",
+  MPLUSBlack: "Rounded Mplus 1c Black",
+
+
+  /******* */
+  //UNCOMMENT FOR ANDROID
+  /**** */
+  // MPLUSThin: "MPLUSRounded1c-Thin",
+  // MPLUSRegular: "MPLUSRounded1c-Regular",
+  // MPLUSBold: "MPLUSRounded1c-Bold",
+  // MPLUSMedium: "MPLUSRounded1c-Medium",
+  // MPLUSBlack: "MPLUSRounded1c-Black"
 }
 
 export { Fonts }
