6a8cbc767f727704379cb8987c8714031d7f1fd1
michael_vons <mike2serg@gmail.com>
2019-01-28 21:32:49 +0100

    font adjustment for android

--- src/util/Fonts.js
+++ src/util/Fonts.js
@@ -2,21 +2,21 @@ const Fonts = {
   /******* */
   //UNCOMMENT FOR IOS
   /**** */
-  MPLUSThin: "Rounded Mplus 1c Thin",
-  MPLUSRegular: "Rounded Mplus 1c",
-  MPLUSBold: "Rounded Mplus 1c Bold",
-  MPLUSMedium: "Rounded Mplus 1c Medium",
-  MPLUSBlack: "Rounded Mplus 1c Black",
+  // MPLUSThin: "Rounded Mplus 1c Thin",
+  // MPLUSRegular: "Rounded Mplus 1c",
+  // MPLUSBold: "Rounded Mplus 1c Bold",
+  // MPLUSMedium: "Rounded Mplus 1c Medium",
+  // MPLUSBlack: "Rounded Mplus 1c Black",
 
 
   /******* */
   //UNCOMMENT FOR ANDROID
   /**** */
-  // MPLUSThin: "MPLUSRounded1c-Thin",
-  // MPLUSRegular: "MPLUSRounded1c-Regular",
-  // MPLUSBold: "MPLUSRounded1c-Bold",
-  // MPLUSMedium: "MPLUSRounded1c-Medium",
-  // MPLUSBlack: "MPLUSRounded1c-Black"
+  MPLUSThin: "MPLUSRounded1c-Thin",
+  MPLUSRegular: "MPLUSRounded1c-Regular",
+  MPLUSBold: "MPLUSRounded1c-Bold",
+  MPLUSMedium: "MPLUSRounded1c-Medium",
+  MPLUSBlack: "MPLUSRounded1c-Black"
 }
 
 export { Fonts }
